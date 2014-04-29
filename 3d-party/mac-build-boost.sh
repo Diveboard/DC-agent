c1c4d0bbad0927f36bb02348e32aaea3b5d6c950
Admin <admin@Mac-Admin.local>
2014-04-29 12:47:59 +0400

    fix some temporary commented out lines

--- 3d-party/mac-build-boost.sh
+++ 3d-party/mac-build-boost.sh
@@ -3,12 +3,12 @@ CURRENTPATH=`pwd`
 arch_flags="-arch i386"
 set -e
 LIBBOOST="boost_1_55_0"
-#if [ -e "${CURRENTPATH}/${LIBBOOST}" ]; then
-#	rm -rf "${CURRENTPATH}/${LIBBOOST}"
-#fi
-#tar zxf ${LIBBOOST}.tar.gz
+if [ -e "${CURRENTPATH}/${LIBBOOST}" ]; then
+	rm -rf "${CURRENTPATH}/${LIBBOOST}"
+fi
+tar zxf ${LIBBOOST}.tar.gz
 cd "${CURRENTPATH}/${LIBBOOST}"
-#./bootstrap.sh
+./bootstrap.sh
 ./b2 install --prefix="${CURRENTPATH}/lib-32" link=static cflags="$arch_flags" cxxflags="$arch_flags -stdlib=libstdc++" linkflags="$arch_flags -stdlib=libstdc++" --without-context --without-coroutine
 cd ${CURRENTPATH}
 echo "Done"
