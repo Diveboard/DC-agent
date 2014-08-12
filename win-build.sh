739bb88b56c57bd3463b703ad4fdc7848cace6b9
Baptiste <baptiste@diveboard.com>
2014-08-12 07:31:55 -0700

    update compilation script

--- win-build.sh
+++ win-build.sh
@@ -6,9 +6,12 @@ echo "BUILD 3D-PARTY"
 ./win-build-all.sh
 cd ${CURRENTPATH}
 echo "BUILD CMAKE"
+mkdir build
+cd build
 cmake -DCMAKE_TOOLCHAIN_FILE=./3d-party/windows-build/Toolchain-mingw32.cmake -DCMAKE_SYSTEM_NAME=Windows
 echo "BUILD PROJECT"
 make
+cd ..
 echo "BUILD SETUP"
 makensis Win/Diveboard.nsi
 echo "Done"
