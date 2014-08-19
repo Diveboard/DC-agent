37fcd35642c57ef1537e2d24334897e0926d4cda
Baptiste <baptiste@diveboard.com>
2014-08-19 12:02:37 +0200

    update comilation script

--- 3d-party/lin-build-openssl.sh
+++ 3d-party/lin-build-openssl.sh
@@ -8,8 +8,7 @@ if [ -e "${CURRENTPATH}/${OPENSSL}" ]; then
 fi
 tar zxf ${OPENSSL}.tar.gz
 cd "${CURRENTPATH}/${OPENSSL}"
-./config shared --prefix="${CURRENTPATH}/lib-32/ssl"
-#./config --prefix="${CURRENTPATH}/lib-32"
+./config shared --prefix="${CURRENTPATH}/lib-32/"
 make
 make install
 cd ${CURRENTPATH}
