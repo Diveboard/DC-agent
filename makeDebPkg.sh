edc909ae0f0c7a2c1e4b58841d211ef951226c52
Baptiste <baptiste@diveboard.com>
2014-08-20 13:28:27 +0200

    add the script to build a deb file

---
+++ makeDebPkg.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+
+DIR=$(dirname $(readlink -f $0))
+
+if [ `arch` == 'x86_64' ]; then
+ARCH=x86_64
+ARCHITECTURE=amd64
+else
+ARCH=i386
+ARCHITECTURE=i386
+fi
+
+VERSION=`cat "$DIR/VERSION"`
+
+
+BINDIR=$DIR/build/bin
+BUILDDIR=$BINDIR/deb
+OUTDIR=$DIR/build/packages
+
+LIBDIVE=$BINDIR/libdivecomputer.so
+APPDIVEBOARD=$BINDIR/DiveboardAgent
+
+PKGNAME=diveboard-agent-$VERSION-$ARCH.deb
+
+INSTALLED_SIZE=`du -bs '$LIBDIVE' '$APPDIVEBOARD' | awk 'BEGIN {s=0} {s+=\$1} END {print s}'`
+
+CONTROL="Package:diveboard-agent
+Version: $VERSION
+Section: web
+Priority: optional
+Architecture: $ARCHITECTURE
+Depends: libc6 (>= 2.11)
+Replaces: diveboard (<= 1.1.1)
+Maintainer: Diveboard <support@diveboard.com>
+Installed-Size: $INSTALLED_SIZE
+Description: Web Browser plugin for DiveBoard:
+ http://www.diveboard.com
+ .
+ With the DiveBoard browser plugin you can upload your dive
+ profiles direct from a supported dive computer to the web.
+ This plugin links with Jef Driesen's LibDiveComputer http://www.divesoftware.org/libdc/
+
+"
+
+#
+# Put the package together
+#
+
+if [ ! -f $APPDIVEBOARD ]; then
+    echo Unable to find DiveBoard - did you build it?
+    echo MISSING: $APPDIVEBOARD
+    exit 1
+fi
+if [ ! -f $LIBDIVE ]; then
+    echo Unable to find libdivecomputer - did you build it?
+    echo MISSING: $LIBDIVE
+    exit 1
+fi
+
+echo Creating build structure...
+mkdir -p $OUTDIR
+rm -f $OUTDIR/$PKGNAME
+rm -rf $BUILDDIR
+mkdir -p $BUILDDIR/usr/lib/diveboard
+mkdir -p $BUILDDIR/usr/bin/
+
+echo Copying package contents...
+cp $APPDIVEBOARD $BUILDDIR/usr/bin/DiveboardAgent
+cp $LIBDIVE $BUILDDIR/usr/lib/diveboard/libdivecomputer.so
+
+echo Stripping binaries...
+strip $BUILDDIR/usr/bin/DiveboardAgent
+strip $BUILDDIR/usr/lib/diveboard/libdivecomputer.so
+
+echo Fixing permissions...
+chmod 755 $BUILDDIR/usr/bin/DiveboardAgent
+chmod 644 $BUILDDIR/usr/lib/diveboard/libdivecomputer.so
+
+echo Creating control file...
+mkdir -p $BUILDDIR/DEBIAN
+echo -e "$CONTROL" > $BUILDDIR/DEBIAN/control
+
+echo Creating postinst script...
+cat > $BUILDDIR/DEBIAN/postinst <<EOF
+#!/bin/bash
+
+set -e
+
+
+exit 0
+ EOF
+EOF
+
+chmod 755 $BUILDDIR/DEBIAN/postinst
+
+echo Creating prerm script...
+cat > $BUILDDIR/DEBIAN/prerm <<EOF
+#!/bin/bash
+
+set -e
+
+exit 0
+ EOF
+EOF
+chmod 755 $BUILDDIR/DEBIAN/prerm
+
+echo Building Package...
+fakeroot dpkg-deb --build $BUILDDIR $OUTDIR/$PKGNAME
+
+if [ ! -f $OUTDIR/$PKGNAME ];then
+    echo Something went wrong!
+    exit 1
+fi
+rm -rf $BUILDDIR
