51b660ba4445bf5ac9fe3135e63e8923a35bdeb3
Alexander <alex@ksso.net>
2014-09-14 22:43:31 +0200

    muf

--- 3d-party/lin-build-wxwidgets.sh
+++ 3d-party/lin-build-wxwidgets.sh
@@ -6,7 +6,7 @@ WXWIDGETS="wxWidgets-3.0-trunk"
 if [ -e "${CURRENTPATH}/${WXWIDGETS}" ]; then
 	rm -rf "${CURRENTPATH}/${WXWIDGETS}"
 fi
-tar xjvf ${WXWIDGETS}.tar.bz2
+tar xzvf ${WXWIDGETS}.tar.bz2
 mkdir -p "${CURRENTPATH}/${WXWIDGETS}/build-32" 
 cd "${CURRENTPATH}/${WXWIDGETS}/build-32"
 ../configure CFLAGS="$arch_flags" CXXFLAGS="$arch_flags" CPPFLAGS="$arch_flags" LDFLAGS="$arch_flags" OBJCFLAGS="$arch_flags" --disable-shared --enable-unicode --enable-stl --with-libjpeg=builtin --with-libpng=builtin --with-regex=builtin --with-libtiff=builtin --with-zlib=builtin --with-expat=builtin --enable-webkit --enable-webview --enable-webviewwebkit --with-opengl --prefix="${CURRENTPATH}/lib-32/wxWidgets-3.0"
