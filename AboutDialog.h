733c51324d9b2d1c4a212548e5c641892634fd12
Hong <hongtranod15@gmail.com>
2014-09-17 09:43:14 +0700

    update the text on the about window as Version xxx build YYYYY

--- AboutDialog.h
+++ AboutDialog.h
@@ -1,10 +1,15 @@
 #ifndef __ABOUT_DIALOG_HPP__
 #define __ABOUT_DIALOG_HPP__
 #include "dive_agent_forms.h"
+#include "Global.h"
 
 class AboutDilog:public AboutDialogBase
 {
 public:
-  AboutDilog():AboutDialogBase(0){};
+  AboutDilog():AboutDialogBase(0)
+  {
+  	m_staticText12->SetLabelText("Version " APPVERSION " build #" VERSION "\nCopyright © 2014 Diveboard SARL or its subsidiaries\n\nDiveboard Agent detect compatible dive computers connected to the\ncomputer and transfers available dive profiles to \n");
+  };
 };
+
 #endif//__ABOUT_DIALOG_HPP__
