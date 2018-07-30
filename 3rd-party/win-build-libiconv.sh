aab7e339eb689ae95466ecd4e393b5ef063452be
Thomas E. Horner <horner@matrix.t-horner.com>
2018-07-30 18:02:52 +0200

    patch to libiconv for windows
    see http://savannah.gnu.org/bugs/?47953

--- 3rd-party/win-build-libiconv.sh
+++ 3rd-party/win-build-libiconv.sh
@@ -9,6 +9,7 @@ fi
 tar zxf ${LIBICONV}.tar.gz
 cd "${CURRENTPATH}/${LIBICONV}"
 ./configure --prefix="${CURRENTPATH}/lib-32" --host=i686-w64-mingw32 --build=i686-linux CFLAGS="$arch_flags" CXXFLAGS="$arch_flags" CPPFLAGS="$arch_flags" LDFLAGS="$arch_flags" OBJCFLAGS="$arch_flags" --enable-shared=yes --enable-static=no
+patch lib/iconv.c ../windows-build/libiconv.patch
 make
 make install
 cd ${CURRENTPATH}
