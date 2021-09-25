7e1835a89f117dced44145eeaee3d7154ec38666
Thomas E. Horner <>
2021-09-25 17:26:50 +0200

    enable building on 20.04.3 LTS
    updated boost to 1.77
    split boost 1.77 into two smaller parts
    added harfbuzz include path

--- 3rd-party/lin-build-boost.sh
+++ 3rd-party/lin-build-boost.sh
@@ -2,14 +2,15 @@
 CURRENTPATH=`pwd`
 arch_flags=""
 set -e
-LIBBOOST="boost_1_68_0"
+LIBBOOST="boost_1_77_0"
 if [ -e "${CURRENTPATH}/${LIBBOOST}" ]; then
 	rm -rf "${CURRENTPATH}/${LIBBOOST}"
 fi
-tar xjf ${LIBBOOST}.tar.bz2 --exclude doc/html
+tar xjf ${LIBBOOST}-src.tar.bz2 --exclude doc/html
+tar xjf ${LIBBOOST}-libs.tar.bz2 --exclude doc/html
 cd "${CURRENTPATH}/${LIBBOOST}"
 ./bootstrap.sh
-./b2 install --prefix="${CURRENTPATH}/lib-32" link=static --without-context --without-coroutine --without-atomic --without-chrono --without-date_time --without-exception --without-filesystem --without-graph --without-graph_parallel --without-iostreams --without-locale --without-log --without-math --without-mpi --without-python --without-program_options --without-random --without-regex --without-serialization --without-signals --without-test --without-timer --without-wave
+./b2 install --prefix="${CURRENTPATH}/lib-32" link=static --without-context --without-coroutine --without-atomic --without-chrono --without-date_time --without-exception --without-filesystem --without-graph --without-graph_parallel --without-iostreams --without-locale --without-log --without-math --without-mpi --without-python --without-program_options --without-random --without-regex --without-serialization --without-test --without-timer --without-wave
 cd ${CURRENTPATH}
 echo "Done"
 
--- 3rd-party/win-build-boost.sh
+++ 3rd-party/win-build-boost.sh
@@ -2,12 +2,12 @@
 CURRENTPATH=`pwd`
 arch_flags=""
 set -e
-LIBBOOST="boost_1_68_0"
+LIBBOOST="boost_1_77_0"
 if [ -e "${CURRENTPATH}/${LIBBOOST}" ]; then
 	rm -rf "${CURRENTPATH}/${LIBBOOST}"
 fi
-tar xjf ${LIBBOOST}.tar.bz2 --exclude doc/html
-
+tar xjf ${LIBBOOST}-src.tar.bz2 --exclude doc/html
+tar xjf ${LIBBOOST}-libs.tar.bz2 --exclude doc/html
 cd "${CURRENTPATH}/${LIBBOOST}"
 ./bootstrap.sh
 echo "using gcc : 4.4 : i686-w64-mingw32-g++
@@ -16,7 +16,7 @@ echo "using gcc : 4.4 : i686-w64-mingw32-g++
         <archiver>i686-w64-mingw32-ar
         <address-model>32
 ;" > user-config.jam
-./b2 address-model=32 toolset=gcc target-os=windows variant=release threading=multi threadapi=win32 --prefix="${CURRENTPATH}/lib-32" link=static --without-context --without-coroutine --without-atomic --without-chrono --without-date_time --without-exception --without-filesystem --without-graph --without-graph_parallel --without-iostreams --without-locale --without-log --without-math --without-mpi --without-python --without-program_options --without-random --without-regex --without-serialization --without-signals --without-test --without-timer --without-wave --user-config=user-config.jam install
+./b2 address-model=32 toolset=gcc target-os=windows variant=release threading=multi threadapi=win32 --prefix="${CURRENTPATH}/lib-32" link=static --without-context --without-coroutine --without-atomic --without-chrono --without-date_time --without-exception --without-filesystem --without-graph --without-graph_parallel --without-iostreams --without-locale --without-log --without-math --without-mpi --without-python --without-program_options --without-random --without-regex --without-serialization --without-test --without-timer --without-wave --user-config=user-config.jam install
 cd ${CURRENTPATH}
 mv lib-32/lib/libboost_thread_win32.a lib-32/lib/libboost_thread.a
 echo "Done"
